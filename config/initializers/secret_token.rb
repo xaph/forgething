1d3f31e5286a2a9a9b5a09b204f835d9e7d4ca8a
Zafer Cakmak <zafer@cloudarena.com>
2012-08-19 12:35:47 +0300

    initial commit. rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forgething::Application.config.secret_token = 'dc1360ae62f8e1c5898deb993abe6a449fa488774154132779186d9723960c652e3df743832345d2d8ec99081704da62d5c03342e3de4d13ad1a3e1bfc976fae'
