911a660944b4fcba16582d2a949bb1da91cf319e
Zafer Cakmak <zafer@cloudarena.com>
2012-08-29 02:54:03 +0300

    added forgetten ability file

---
+++ app/models/ability.rb
@@ -0,0 +1,26 @@
+# ForgeThing - Simple todo app
+# Copyright (C) 2012  Zafer Cakmak
+# 
+# This file is part of ForgeThing.
+# 
+# ForgeThing is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ForgeThing is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ForgeThing.  If not, see <http://www.gnu.org/licenses/>.
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    can :manage, Todo, :user_id => user.id
+  end
+end
