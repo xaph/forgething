561e62f72137c0bcef989bfcb8b041119acef6d8
Ender Ahmet Yurt <enderyurt@gmil.com>
2012-09-02 15:30:34 +0300

    fix #27

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ActiveRecord::Base
   attr_accessible :name
 
   validates_presence_of :name
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, :scope => :user_id, :message => "must be unique"
 
   has_and_belongs_to_many :todos
   belongs_to :user
