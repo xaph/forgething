262cc2d38e2661c98e968d3824f088b767487824
Tore Serter <me@toreserter.com>
2012-09-02 17:44:37 +0300

    controller tests added

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,10 +1,23 @@
-require "rspec"
+require "spec_helper"
 
-describe "My behaviour" do
+describe HomeController do
 
-  it "should do something" do
-
-    #To change this template use File | Settings | File Templates.
-    true.should == false
+  before do
+    @user = FactoryGirl.create(:user)
   end
+
+  context "#index" do
+
+    it "should redirect to todos_url" do
+      sign_in @user
+      get :index
+      response.should redirect_to todos_url
+    end
+    it "should not redirect to todos_url if user not logged in" do
+      get :index
+      response.should_not redirect_to todos_url
+    end
+
+  end
+
 end
